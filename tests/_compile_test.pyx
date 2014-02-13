9e25b101b8263b42d98581ac123b501eea2cd435
andreas heger <andreas.heger@gmail.com>
2014-02-13 21:12:23 +0000

    completed move to github

--- tests/_compile_test.pyx
+++
@@ -1,6 +0,0 @@
-from pysam.csamtools cimport Samfile
-from pysam.ctabix cimport Tabixfile
-
-cdef Samfile samfile
-cdef Tabixfile tabixfile
-
--- tests/compile_test.py
+++
@@ -1,6 +0,0 @@
-'''test script for checking if compilation against
-pysam and tabix works.'''
-
-import pyximport
-pyximport.install( build_in_temp=False)
-import _compile_test
